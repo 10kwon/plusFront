7c75200ebb65721397ab92dd3aad5e61c2a2ba7e
Lee Maneon <support@pcor.me>
2023-10-09 14:45:45 +0000

    피그마 링크 추가

--- src/pages/promotion/231008.jsx
+++ src/pages/promotion/231008.jsx
@@ -48,16 +48,17 @@ export const Event231008Page = (props) => {
         <div class="max-w-screen-xl mx-auto">
         <h2 className="text-4xl font-bold tracking-tight text-gray-100 sm:text-6xl">신청 절차</h2>
           <p className="mt-6 text-lg leading-8 text-gray-100">
-            아래 공모 양식 다운로드 후 카드를 디자인 하여 메일을 보내 주세요.
+            아래 공모 양식 다운로드 후 카드를 디자인 하여 메일을 보내 주세요.<br/>
+            Word 2013 이상 버전으로 실행해야 제출 문서가 정상적으로 표시돼요.
           </p>
           <div class="flex">
           <a href="/apply.docx"
 class="mt-5 px-3 mr-2 bg-blue-500 font-bold text-white text-center py-2 rounded-xl transform ease-in duration-100 active:scale-95 hover:bg-blue-600">
-제출용 문서 (DOCX)
+제출용 문서 (*.DOCX)
 </a>
-          <a href="https://imagestorage.pcor.me/images/2023/10/08/200--2_20231008230941.png"
+          <a href="https://www.figma.com/community/file/1293222287316295832/pluscoin"
 class="mt-5 px-3 mr-2 bg-blue-500 font-bold text-white text-center py-2 rounded-xl transform ease-in duration-100 active:scale-95 hover:bg-blue-600">
-디자인 가이드
+디자인 가이드 (Figma)
 </a>
           <a href="mailto:support@pcor.me"
 class="mt-5 px-3 mr-2 bg-gray-500 font-bold text-white text-center py-2 rounded-xl transform ease-in duration-100 active:scale-95 hover:bg-gray-600">
