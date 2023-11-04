d87dfe38fa6e2f91b1f95c89d758b251e581d588
Lee Maneon <support@pcor.me>
2023-11-04 13:37:54 +0000

    데코 설정 추가

--- src/pages/app/card.jsx
+++ src/pages/app/card.jsx
@@ -31,13 +31,45 @@ if (props.isLogin == 0){
       
         <Header isLogin={props.isLogin} userName={props.userName} userEmail={props.userEmail} userImage={props.userImage} userCoin={props.userCoin}  userCash={props.userCash} />
 
-<div class="min-h-screen">
+        <div class="border-b border-transparent pt-1 pb-1 md:px-4">
+<Tab.Group>
+      <Tab.List className="w-full -mb-px flex space-x-8 ">
+        <Tab className={({ selected }) =>
+                            classNames(
+                              selected ? 'border-blue-500 text-blue-500 dark:border-blue-300 dark:text-blue-300' : 'border-transparent text-gray-900 dark:text-gray-100',
+                              'flex-1 whitespace-nowrap border-b-2 px-1 py-4 text-base font-medium'
+                            )
+                          }>등록카드</Tab>
+        <Tab className={({ selected }) =>
+                            classNames(
+                              selected ? 'border-blue-500 text-blue-500 dark:border-blue-300 dark:text-blue-300' : 'border-transparent text-gray-900 dark:text-gray-100',
+                              'flex-1 whitespace-nowrap border-b-2 px-1 py-4 text-base font-medium'
+                            )
+                          }>데코</Tab>
+      </Tab.List>
+      <Tab.Panels>
+        <Tab.Panel>
+        <div class="min-h-screen mb-8">
         <Iframe url="https://mapi.pcor.me/profilecard.php"
         id="iframeId"
         className="w-full h-screen"
         display="block"
         position="relative"/>
 	</div>
+        </Tab.Panel>
+        <Tab.Panel>
+        <div class="min-h-screen mb-8">
+        <Iframe url="https://mapi.pcor.me/profiledeco.php"
+        id="iframeId"
+        className="w-full h-screen"
+        display="block"
+        position="relative"/>
+	</div>
+        </Tab.Panel>
+      </Tab.Panels>
+    </Tab.Group>
+</div>
+
         <Footer/>
         
    </div>
